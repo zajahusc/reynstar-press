dd3bc740609aa7fee09e6daf42cb7101f4e5da8b
zajahusc <zajahusc@gmail.com>
2026-08-24 12:45:10 -0500

    Connect newsletter form to email

--- script.js
+++ script.js
@@ -20,23 +20,13 @@ if (yearElement) {
 }
 
 document.querySelector('.newsletter-form')?.addEventListener('submit', (event) => {
-  event.preventDefault();
-
   const button = event.currentTarget.querySelector('button');
   const input = event.currentTarget.querySelector('input');
 
   if (button) {
-    const originalText = button.textContent;
-    button.textContent = 'Joined';
+    button.textContent = 'Sending...';
     button.disabled = true;
-
-    if (input) {
-      input.value = '';
-    }
-
-    window.setTimeout(() => {
-      button.textContent = originalText;
-      button.disabled = false;
-    }, 1800);
   }
+
+  if (input) input.disabled = true;
 });
